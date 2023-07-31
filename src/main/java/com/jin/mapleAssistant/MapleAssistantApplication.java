9d1b6428262b92ee7db4ac4dfea56a7c0c857804
jinhyoung kim <jinhyoung.kim@fdmgroup.com>
2023-07-31 15:23:17 -0400

    Initial commit

---
+++ src/main/java/com/jin/mapleAssistant/MapleAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.jin.mapleAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapleAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jin/mapleAssistant/MapleAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.mapleAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
